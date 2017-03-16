fe6528ab7b596ccd72df276a35b6d7b31e5dd575
Mark Powell <hi@powellmark.com>
2017-03-16 19:53:07 +1100

    DOh, forgot to remove pry and sinatra/reloader before deployment

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
-require 'pry'
+# require 'pry'
 require 'sinatra'
-require 'sinatra/reloader'
+# require 'sinatra/reloader'
 require 'pg'
 require_relative 'database_config'
 require_relative 'models/shop'
